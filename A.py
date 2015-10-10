762ffdc27367301a06bebb77dd3f4cca4a8171fc
KedaloEM <kedaloem@yandex.ru>
2015-10-11 00:09:39 +0300

    Please enter the commit message for your changes. Lines starting

--- A.py
+++ A.py
@@ -1,12 +1,15 @@
-n=int(input())
-A=input().split()
-for i in range(len(A)):
-    A[i]=int(A[i])
+input = open('input.txt', 'r')
+A=[] 
+B=intinput.readlines() 
+n=int(B[0]) 
+A=A+list(map(int, B[1].split())) 
 for i in range (n):
     D=A[i]
     for j in range(i+1,n):
         C=A[i]
         if D==C:
             z=D
-print(z)            
-        
+output = open('output.txt', 'w')
+print(z,file=output)
+input.close()
+output.close()
